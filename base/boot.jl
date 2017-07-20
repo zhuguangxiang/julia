1545a701f6a99ebdd15b122d552541b34f8110e2
Jameson Nash <vtjnash@gmail.com>
2017-07-20 17:12:17 -0400

    allow tasks to request dedicated stack space when created
    never copy over the root stack:
    this is a hybrid approach to COPY_STACK where the root task is never
    moved or copied, and all other task stacks are layered into the same
    memory area (ptls->basestack + basesize)
    
    several strategies exist for making new stacks:
    ucontext_t (where it is available, aka linux)
    unw_context_t (as an alternative to ucontext_t that avoids a syscall on task-switch)
    makecontext (as a posix standard implemention)
    setjmp/longjmp-based implementation (for systems where this is sufficient)
    Windows Fibers (implemented here, since we can be more efficient and
        reliable than the official Fibers API)
    
    also, uses an alternate stack for use in collecting stack-overflow backtraces
    like posix, but managed manually

--- base/boot.jl
+++ base/boot.jl
@@ -370,7 +370,9 @@ eval(Core, :(UpsilonNode() = $(Expr(:new, :UpsilonNode))))
 
 Module(name::Symbol=:anonymous, std_imports::Bool=true) = ccall(:jl_f_new_module, Ref{Module}, (Any, Bool), name, std_imports)
 
-Task(@nospecialize(f)) = ccall(:jl_new_task, Ref{Task}, (Any, Int), f, 0)
+function Task(@nospecialize(f), reserved_stack::Int=0)
+    return ccall(:jl_new_task, Ref{Task}, (Any, Int), f, reserved_stack)
+end
 
 # simple convert for use by constructors of types in Core
 # note that there is no actual conversion defined here,
